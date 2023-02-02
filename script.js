278ee9575eaec95b01ac30a1836e5e5a8ca014c9
Ameer Hamza <ameer.hamza102002@gmail.com>
2023-02-02 10:33:47 +0500

    commited

---
+++ script.js
@@ -0,0 +1,8 @@
+function rotate(){
+    var output=document.getElementById("input").value;
+    //  var arr=output.split('');
+    for(let i=output.length-1;i>=0;i--){
+        console.log(output[i]);
+    }
+    
+}
